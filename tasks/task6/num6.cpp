27460842b057c4643f7b3af9b1b942434ceed1ce
YeXieYe <your.email@example.com>
2025-06-19 16:58:11 +0800

    Update changes in tasks folder

--- tasks/task6/num6.cpp
+++ tasks/task6/num6.cpp
@@ -6,6 +6,7 @@ int main()
     int n = 0;
     std::cin >> n;
     int res = 0;
+    std::vector<int> results;
     for (int i = 0; i < n; i++) {
         int a, b, x, y;
         std::cin >> a >> b >> x >> y;
@@ -13,7 +14,10 @@ int main()
         int remaining_x = x - B_pairs;
         int A_pairs = std::min(a, std::max(0, remaining_x) + y);
         int total_pairs = B_pairs + A_pairs;
-        std::cout << total_pairs << " " << std::endl;
+        results.push_back(total_pairs);
+    }
+    for (int i = 0; i < n; i++) {
+        std::cout << results[i] << " ";
     }
     return 0;
 }
