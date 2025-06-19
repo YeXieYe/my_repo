1908c3729672ff2600acd6dd87db27c447c095d1
YeXieYe <your.email@example.com>
2025-06-19 17:26:02 +0800

    Update changes in tasks folder

--- tasks/task3/num3.cpp
+++ tasks/task3/num3.cpp
@@ -5,21 +5,25 @@ int main() {
   int n = 0;
   int k = 0;
   std::cin >> n >> k;
-  std::vector<int> vec(n);
+  std::vector<int> vec(n, 0);
+  std::vector<int> output;
   for (int i = 0; i < k; i++) {
     int request = 0;
     int a = 0;
     int b = 0;
     std::cin >> request >> a >> b;
     if (request == 1) {
-      vec[a] = b;
+      vec[a - 1] = b;
     } else if (request == 2) {
       int res = 0;
       for (int j = a; j <= b; j++) {
-        res += vec[j];
+        res += vec[j - 1];
       }
-      std::cout << res << std::endl;
+      output.push_back(res);
     }
+  }
+  for (int res : output) {
+    std::cout << res << std::endl;
   }
   return 0;
 }
